0a708255186fedb89f6d058e6c337c66fbd34c00
Luquilmer Capita <luedihg@gmail.com>
2025-10-22 23:45:44 +0100

    Refactor HojeLB class to TeamAngola_TeleOp2025

--- HojeLB.java
+++ HojeLB.java
@@ -6,112 +6,129 @@ import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp(name = "HojeLB (Blocks to Java)")
-public class HojeLB extends LinearOpMode {
+@TeleOp(name = "TeamAngola_TeleOp2025")
+public class TeamAngola_TeleOp2025 extends LinearOpMode {
 
-  private DcMotor MD0;
-  private DcMotor ME1;
-  private DcMotor Sug;
-  private DcMotor Sug2;
-  private DcMotor MPenD0;
-  private DcMotor MPenE1;
-  private DcMotor Ace;
-  private CRServo Srigth;
-  private CRServo Sleft;
-  private Servo Braleft;
-  private Servo Braright;
+    private DcMotor MD0;
+    private DcMotor ME1;
+    private DcMotor Brac;
+    private DcMotor Sug;
+    private DcMotor Sug2;
+    private DcMotor MPenD0;
+    private DcMotor MPenE1;
+    private DcMotor Ace;
+    private CRServo Sright;
+    private CRServo Sleft;
+    private Servo Braleft;
+    private Servo Braright;
 
-  /**
-   * This sample contains the bare minimum Blocks for any regular OpMode. The 3 blue
-   * Comment Blocks show where to place Initialization code (runs once, after touching the
-   * DS INIT button, and before touching the DS Start arrow), Run code (runs once, after
-   * touching Start), and Loop code (runs repeatedly while the OpMode is active, namely not
-   * Stopped).j.
-   */
-  @Override
-  public void runOpMode() {
-    float x;
-    float y;
-    float Power0;
-    float Power1;
+    @Override
+    public void runOpMode() {
+        float x;
+        float y;
+        float Power0;
+        float Power1;
+        float Powerbrac;
 
-    MD0 = hardwareMap.get(DcMotor.class, "MD0");
-    ME1 = hardwareMap.get(DcMotor.class, "ME1");
-    Sug = hardwareMap.get(DcMotor.class, "Sug");
-    Sug2 = hardwareMap.get(DcMotor.class, "Sug2");
-    MPenD0 = hardwareMap.get(DcMotor.class, "MPenD0");
-    MPenE1 = hardwareMap.get(DcMotor.class, "MPenE1");
-    Ace = hardwareMap.get(DcMotor.class, "Ace");
-    Srigth = hardwareMap.get(CRServo.class, "Srigth");
-    Sleft = hardwareMap.get(CRServo.class, "Sleft");
-    Braleft = hardwareMap.get(Servo.class, "Braleft");
-    Braright = hardwareMap.get(Servo.class, "Braright");
+        // Initialize hardware mapping
+        MD0 = hardwareMap.get(DcMotor.class, "MD0");
+        ME1 = hardwareMap.get(DcMotor.class, "ME1");
+        Brac = hardwareMap.get(DcMotor.class, "Brac");
+        Sug = hardwareMap.get(DcMotor.class, "Sug");
+        Sug2 = hardwareMap.get(DcMotor.class, "Sug2");
+        MPenD0 = hardwareMap.get(DcMotor.class, "MPenD0");
+        MPenE1 = hardwareMap.get(DcMotor.class, "MPenE1");
+        Ace = hardwareMap.get(DcMotor.class, "Ace");
+        Sright = hardwareMap.get(CRServo.class, "Sright");
+        Sleft = hardwareMap.get(CRServo.class, "Sleft");
+        Braleft = hardwareMap.get(Servo.class, "Braleft");
+        Braright = hardwareMap.get(Servo.class, "Braright");
 
-    // Put initialization blocks here.
-    waitForStart();
-    if (opModeIsActive()) {
-      // Put run blocks here.
-      MD0.setDirection(DcMotor.Direction.REVERSE);
-      ME1.setDirection(DcMotor.Direction.FORWARD);
-      while (opModeIsActive()) {
-        // Put loop blocks here.
-        x = -gamepad1.left_stick_x;
-        y = -gamepad1.left_stick_y;
-        Power0 = y - x;
-        Power1 = x + y;
-        MD0.setPower(Power0);
-        ME1.setPower(Power1);
-        if (gamepad1.right_bumper) {
-          Sug.setPower(-1);
-          Sug2.setPower(1);
-        } else {
-          Sug.setPower(0);
-          Sug2.setPower(0);
+        // Reverse one side for tank drive
+        MD0.setDirection(DcMotor.Direction.REVERSE);
+        ME1.setDirection(DcMotor.Direction.FORWARD);
+
+        waitForStart();
+
+        if (opModeIsActive()) {
+            while (opModeIsActive()) {
+                // Main drive control (arcade style)
+                x = -gamepad1.left_stick_x;
+                y = -gamepad1.left_stick_y;
+                Power0 = y - x;
+                Power1 = y + x;
+                MD0.setPower(Power0);
+                ME1.setPower(Power1);
+
+                // Arm control
+                x = -gamepad1.right_stick_x;
+                y = -gamepad1.right_stick_y;
+                Powerbrac = y;
+                Brac.setPower(Powerbrac);
+
+                // Intake forward
+                if (gamepad1.right_bumper) {
+                    Sug.setPower(-1);
+                    Sug2.setPower(1);
+                } 
+                // Intake reverse
+                else if (gamepad1.left_bumper) {
+                    Sug.setPower(1);
+                    Sug2.setPower(-1);
+                } 
+                // Stop intake
+                else if (gamepad1.dpad_up) {
+                    Sug.setPower(0);
+                    Sug2.setPower(0);
+                }
+
+                // Lift down
+                if (gamepad1.cross) {
+                    MPenD0.setPower(1);
+                    MPenE1.setPower(1);
+                } 
+                // Lift up
+                else if (gamepad1.triangle) {
+                    MPenD0.setPower(-1);
+                    MPenE1.setPower(-1);
+                } 
+                // Stop lift
+                else {
+                    MPenD0.setPower(0);
+                    MPenE1.setPower(0);
+                }
+
+                // Actuator control
+                if (gamepad1.circle) {
+                    Ace.setPower(1);
+                } 
+                // Stop actuator & servos
+                else if (gamepad1.square) {
+                    Ace.setPower(0);
+                    Sright.setPower(0);
+                    Sleft.setPower(0);
+                }
+
+                // Servo rotation (continuous)
+                if (gamepad1.dpad_up) {
+                    Sright.setPower(1);
+                    Sleft.setPower(-1);
+                } 
+                else if (gamepad1.dpad_down) {
+                    Sleft.setPower(1);
+                    Sright.setPower(-1);
+                }
+
+                // Arm servos (standard)
+                if (gamepad1.dpad_right) {
+                    Braleft.setPosition(130.0 / 180.0);
+                    Braright.setPosition(75.0 / 180.0);
+                } 
+                else if (gamepad1.dpad_left) {
+                    Braleft.setPosition(60.0 / 180.0);
+                    Braright.setPosition(165.0 / 180.0);
+                }
+            }
         }
-        if (gamepad1.left_bumper) {
-          Sug.setPower(1);
-          Sug2.setPower(-1);
-        }
-        if (gamepad1.dpad_up) {
-          Sug.setPower(0);
-          Sug2.setPower(0);
-        }
-        if (gamepad1.cross) {
-          MPenD0.setPower(-1);
-          MPenE1.setPower(-1);
-        } else {
-          MPenD0.setPower(0);
-          MPenE1.setPower(0);
-        }
-        if (gamepad1.triangle) {
-          MPenE1.setPower(1);
-          MPenD0.setPower(1);
-        }
-        if (gamepad1.circle) {
-          Ace.setPower(1);
-        }
-        if (gamepad1.square) {
-          Ace.setPower(0);
-          Srigth.setPower(0);
-          Sleft.setPower(0);
-        }
-        if (gamepad1.dpad_up) {
-          Srigth.setPower(1);
-          Sleft.setPower(-1);
-        }
-        if (gamepad1.dpad_down) {
-          Sleft.setPower(1);
-          Srigth.setPower(-1);
-        }
-        if (gamepad1.dpad_right) {
-          Braleft.setPosition(130 / 180);
-          Braright.setPosition(65 / 180);
-        }
-        if (gamepad1.dpad_left) {
-          Braleft.setPosition(60 / 180);
-          Braright.setPosition(165 / 180);
-        }
-      }
     }
-  }
 }
